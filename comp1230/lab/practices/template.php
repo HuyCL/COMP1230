47d18c1c7f61b722e20d2531ea4511f9a1fc70d1
HuyCL <43223817+HuyCL@users.noreply.github.com>
2023-09-24 22:22:00 -0700

    Add files via upload

---
+++ comp1230/lab/practices/template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">    
+    <title>Template</title>
+    <meta name="description" content="Template">
+    <meta name="author" content="Your name">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <script src=https://my.gblearn.com/js/loadscript.js></script>
+</body>
+</html>
+
+<?php
+    echo "<hr>";
+    show_source(__FILE__);
+?>
